50cb7d385337f2cc8eadc74419659ece25a8e9da
Vidhya Ramamoorthy <devvid8607@gmail.com>
2023-03-24 19:59:03 +1100

    new changes made

---
+++ redux-toolkit/src/features/counter/counterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      console.log(state.count);
+      state.count += 1;
+    },
+    decrement: (state) => {
+      console.log(state.count);
+      state.count -= 1;
+    },
+    reset: (state) => (state.count = 0),
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
