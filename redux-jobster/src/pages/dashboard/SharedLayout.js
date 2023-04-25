bf5f43033d2d318cfc1af3699007b0859772598a
Vidhya Ramamoorthy <devvid8607@gmail.com>
2023-04-26 08:16:51 +1000

    updated

---
+++ redux-jobster/src/pages/dashboard/SharedLayout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Wrapper from "../../assets/wrappers/SharedLayout";
+import { Outlet } from "react-router-dom";
+import { Navbar, SmallSidebar, BigSidebar } from "../../components";
+
+function SharedLayout() {
+  return (
+    <>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
+  );
+}
+
+export default SharedLayout;
---
+++ redux-jobster/src/pages/dashboard/index.js
@@ -0,0 +1,7 @@
+import SharedLayout from "./SharedLayout";
+import AllJobs from "./AllJobs";
+import Stats from "./Stats";
+import Profile from "./Profile";
+import AddJob from "./AddJob";
+
+export { SharedLayout, AllJobs, Stats, Profile, AddJob };
